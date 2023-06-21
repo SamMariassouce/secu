b0e0aaead08780b42f8b07a1c88eaacf94277785
Sam Mariassouce <sammariassouce@gmail.com>
2023-06-20 15:56:28 -1000

    524 fetch registre api ok

--- functions/registre.js
+++ functions/registre.js
@@ -1,6 +1,28 @@
-exports.handler = async (event, context, cb) => {
-  return {
-    statusCode: 200,
-    body: 'registre route',
-  };
+const dotenv = require('dotenv');
+dotenv.config();
+
+const Airtable = require('airtable-node');
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base(process.env.AIRTABLE_BASE)
+  .table(process.env.AIRTABLE_TABLE);
+
+exports.handler = async function () {
+  try {
+    const response = await airtable.list({ maxRecords: 200 });
+
+    console.log('#######');
+    console.log(response);
+    console.log('#######');
+
+    return {
+      statusCode: 200,
+      body: 'products route',
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: 'Il y a eu une erreur',
+    };
+  }
 };
