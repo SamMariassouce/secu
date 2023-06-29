66143be4ede9d8d495429004f2c1f1ce738da968
Sam Mariassouce <sammariassouce@gmail.com>
2023-06-28 18:08:36 -1000

    cli

--- src/pages/PlanPage.js
+++ src/pages/PlanPage.js
@@ -19,7 +19,7 @@ const PlansPage = () => {
             Une pour visionner les plans:
             <br />
             <a href="https://cps-planlow.netlify.app/" target="_blank">
-              clicquez ici
+              cliquez ici
             </a>
           </p>
 
@@ -30,7 +30,7 @@ const PlansPage = () => {
             temps.
             <br />
             <a href="https://cps-plans.netlify.app/" target="_blank">
-              clicquez Là
+              cliquez Là
             </a>
           </p>
         </article>
